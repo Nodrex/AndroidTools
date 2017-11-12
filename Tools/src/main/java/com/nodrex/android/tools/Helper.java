de3bdb4f4bc99e98bf22be92c3e83f0aa8a0285c
Nodar Tchumbadze <Nodar Tchumbadze>
2017-11-12 11:29:16 +0400

    code merging from qarva tools

--- Tools/src/main/java/com/nodrex/android/tools/Helper.java
+++ Tools/src/main/java/com/nodrex/android/tools/Helper.java
@@ -1,11 +1,62 @@
 package com.nodrex.android.tools;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
+
 /**
  * Created by nchum on 11/8/2017.
  */
-
 public class Helper {
 
+    public static final String EMPTY_STRING = "";
+    public static final String MD5 = "MD5";
     public static final String UTF8 = "UTF-8";
+    public static final String SHA1 = "SHA-1";
+    public static final String NEW_LINE = "\n";
+    public static final String NEW_LINE_AND_TAB = "\n\t";
+    public static final String SPACE = " ";
+
+    /**
+     * Extracts ip from host name like google.com
+     * @param hostName
+     * @return ip of hostName or null , if UnknownHostException occurred or hostName is null.
+     */
+    public static String resolveIp(String hostName){
+        if(hostName == null) return null;
+        try {
+            InetAddress address = InetAddress.getByName(hostName);
+            return address.getHostAddress();
+        } catch (UnknownHostException e) {
+            Util.log("can not resolveIp of " + hostName + " :" + e.toString());
+        }
+        return null;
+    }
+
+    /**
+     * Converts number less then 10 in to String number concatenated with 0 before number, like 01,02,03...</br>
+     * and number more then 10 is just converted in to String.
+     * @param num
+     * @return String number.
+     */
+    public static String format02(int num) {
+        if (num < 10) return "0"+num;
+        return num + Helper.EMPTY_STRING;
+    }
+
+    public static boolean isEmptyOrNull(String value){
+        return value == null || "".equals(value) ;
+    }
+
+    /**
+     * generates random number between 0 to max
+     * @param max maximum random number which can be returned from this method.
+     * @return random number between 0 to max
+     */
+    public static int generateRandom(int max){
+        Random rn = new Random();
+        int range = max + 1;
+        return  rn.nextInt(range);
+    }
 
 }
--- Tools/src/main/java/com/nodrex/android/tools/Time.java
+++ Tools/src/main/java/com/nodrex/android/tools/Time.java
@@ -1,13 +1,20 @@
 package com.nodrex.android.tools;
 
+import android.app.Activity;
+import android.content.res.Resources;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class Time {
 
     public static final String UTC = "UTC";
+    private static int [] dateTime = new int[7];//creates only once and then fills when ever needed. is used for pix to load time more faster.
+    private static Calendar c = new GregorianCalendar();//GregorianCalendar is created only once (optimization for pix).
 
     /**
      * Contains time formats.
@@ -68,6 +75,17 @@ public class Time {
     }
 
     /**
+     * Generates current device time zone in hours.
+     * @return timZoneOffsets
+     */
+    public static long getTimeZoneGMT() {
+        TimeZone tz = TimeZone.getDefault();
+        long timeZone = tz.getOffset(new Date().getTime());
+        return TimeUnit.MILLISECONDS.toHours(timeZone);
+    }
+
+
+    /**
      * Generates current device time zone.
      * @param format {@link Format} Long or Short. Long means in milliseconds and Short will return one integer number. for georgia example: +4
      * @return timZoneOffsets
@@ -81,6 +99,24 @@ public class Time {
             case Short:
             default: return TimeUnit.MILLISECONDS.toHours(timeZone);
         }
+    }
+
+    /**
+     * Coverts given time in to human readable string time of hour:minute
+     * @param time witch should be converted.
+     * @return string time in format => hour(24):minute
+     */
+    public static String getTimeHHmm(long time){
+        return new SimpleDateFormat("HH:mm").format(time);
+    }
+
+    /**
+     * Coverts given original time in to human readable string time of houe:minute:second.
+     * @param time witch should be converted.
+     * @return string time in format => hour(24):minute:second
+     */
+    public static String getTimeHHmmss(long time){
+        return new SimpleDateFormat("HH:mm:ss").format(time);
     }
 
     /**
@@ -199,6 +235,82 @@ public class Time {
         c.setTimeZone(TimeZone.getTimeZone(UTC));
         c.set(year,month,day,0,0,0);
         return c.getTimeInMillis();
+    }
+
+
+    /**
+     * converts long time to array fo human readable time like hour,minute,year,month,day of month, second, day of week.
+     * @param time to convert really human readable time
+     * @return array of hour, minute, year, month, day, second, day of week.
+     */
+    public static int[] getTime(long time){
+        c.setTimeInMillis(time);
+        dateTime[0] = c.get(Calendar.HOUR_OF_DAY);
+        dateTime[1] = c.get(Calendar.MINUTE);
+        dateTime[2] = c.get(Calendar.YEAR);
+        dateTime[3] = c.get(Calendar.MONTH);
+        dateTime[4] = c.get(Calendar.DAY_OF_MONTH);
+        dateTime[5] = c.get(Calendar.SECOND);
+        dateTime[6] = c.get(Calendar.DAY_OF_WEEK);
+        return dateTime;
+    }
+
+    /**
+     * Reads String day value from resources depend on int day value.
+     * </br>Returned value depends on locale(Language) configuration of app,
+     * </br>Value is read from ge if language is georgian and so on...
+     * @param activity
+     * @param day
+     * @return String representation of day from int value.
+     */
+    public static String getDayOfWeek(Activity activity, int day){
+        if(activity == null) return null;
+        Resources resources = Util.getResources(activity);
+        if(resources == null) return null;
+        String [] days = resources.getStringArray(R.array.daysOfWeek);
+        if(days == null) return null;
+        switch (day){
+            case Calendar.MONDAY: return days[0];
+            case Calendar.TUESDAY: return days[1];
+            case Calendar.WEDNESDAY: return days[2];
+            case Calendar.THURSDAY: return days[3];
+            case Calendar.FRIDAY: return days[4];
+            case Calendar.SATURDAY: return days[5];
+            case Calendar.SUNDAY: return days[6];
+        }
+        return null;
+    }
+
+    /**
+     * Reads String month value from resources depend on int month value.
+     * </br>Returned value depends on locale(Language) configuration of app,
+     * </br>Value is read from ge if language is georgian and so on...
+     * @param activity
+     * @param month
+     * @param shortMonths
+     * @return String representation of month from int value.
+     */
+    public static String getMonth(Activity activity,int month,boolean shortMonths){
+        if(activity == null) return null;
+        Resources resources = Util.getResources(activity);
+        if(resources == null) return null;
+        String [] months = resources.getStringArray(shortMonths ? R.array.monthsShort : R.array.monthsLong);
+        if(months == null) return null;
+        switch (month){
+            case Calendar.JANUARY: return months[0];
+            case Calendar.FEBRUARY: return months[1];
+            case Calendar.MARCH: return months[2];
+            case Calendar.APRIL: return months[3];
+            case Calendar.MAY: return months[4];
+            case Calendar.JUNE: return months[5];
+            case Calendar.JULY: return months[6];
+            case Calendar.AUGUST: return months[7];
+            case Calendar.SEPTEMBER: return months[8];
+            case Calendar.OCTOBER: return months[9];
+            case Calendar.NOVEMBER: return months[10];
+            case Calendar.DECEMBER: return months[11];
+        }
+        return null;
     }
 
     /**
